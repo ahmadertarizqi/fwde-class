78b8885dffca1c9eaf0d2d632c93cc282583f51a
Ahmaderta Rizqi <ahmadderta.rizqi30@gmail.com>
2021-05-27 13:51:57 +0700

    improve layout

--- src/scripts/views.js
+++ src/scripts/views.js
@@ -2,8 +2,8 @@ import data from '../DATA.json';
 import { icons } from 'feather-icons';
 import { truncateString } from './utilities';
 
-function showRestaurantList() {
-   const parentElement = document.querySelector('.restaurant__list');
+function renderResturantList() {
+   const parentElem = document.querySelector('.restaurant__list');
    const { restaurants } = data;
    
    let markup = '';
@@ -11,25 +11,51 @@ function showRestaurantList() {
       markup = `
          <div class="restaurant__item">
             <div class="restaurant__card">
-               <figure class="restaurant__card-head">
-                  <img src="${restaurant.pictureId}" alt="restaurant-image-item" />
+               <div class="restaurant__card-head">
+                  <figure class="image">
+                     <img src="${restaurant.pictureId}" alt="restaurant-image-item" />
+                  </figure>
                   <span class="rating-item">${icons['star'].toSvg()} ${restaurant.rating}</span>
-               </figure>
+               </div>
                <div class="restaurant__card-body">
                   <span class="location">${icons['map-pin'].toSvg()} ${restaurant.city}</span>
                   <h4 class="name">${restaurant.name}</h4>
-                  <p class="description">${truncateString(restaurant.description, 25, '...')}</p>
+                  <p class="description">${truncateString(restaurant.description, 20, '...')}</p>
                </div>
             </div>
          </div>
       `;
-
-      parentElement.insertAdjacentHTML('beforeend', markup);
+      parentElem.insertAdjacentHTML('beforeend', markup);
    });
-}
+};
 
-document.addEventListener('DOMContentLoaded', function() {
-   // === NAVBAR MENU ===
+function stickyHeader() {
+   const headerElement = document.querySelector('.header');
+   const navbarElement = document.querySelector('.navbar');
+   const navbarHeight = navbarElement.getBoundingClientRect().height;
+
+   const handleIntersection = (entries) => {
+      const [entry] = entries;
+      // console.log(entry.isIntersecting);
+      if(!entry.isIntersecting) {
+         navbarElement.classList.add('is-sticky');
+         navbarElement.classList.remove('is-transparent');
+      } else {
+         navbarElement.classList.remove('is-sticky');
+         navbarElement.classList.add('is-transparent');
+      }
+   };
+
+   const headerObserver = new IntersectionObserver(handleIntersection, {
+      root: null,
+      threshold: 0,
+      rootMargin: `${navbarHeight}px`
+   });
+   headerObserver.observe(headerElement);
+};
+
+
+function navbarMenu() {
    const menuNavbar = document.querySelector('.navbar__menu');
    const btnToggleNavbar = document.querySelector('.navbar--toggler');
    const btnCloseNavbar = document.querySelector('.navbar--close');
@@ -56,7 +82,10 @@ document.addEventListener('DOMContentLoaded', function() {
          menuNavbar.parentNode.removeChild(backdropElements[0]);
       }
    });
-   // === NAVBAR MENU ===
+};
 
-   showRestaurantList();
+document.addEventListener('DOMContentLoaded', function() {
+   navbarMenu();
+   renderResturantList();
+   stickyHeader();
 });
